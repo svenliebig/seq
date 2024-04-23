666e556446c6175a02a6a2d98b761a3d591743b0
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2024-04-23 18:24:38 +0200

    add a general benchmark

---
+++ seq_test.go
@@ -0,0 +1,30 @@
+package seq
+
+import (
+	"testing"
+)
+
+func BenchmarkSeqCreateMapFilterCollect(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Collect(
+			Map(
+				Filter(
+					Int(0, 10000),
+					func(i int) bool { return i%2 == 0 },
+				),
+				func(i int) int { return i * 2 },
+			),
+		)
+	}
+}
+
+func BenchmarkSeqCreatemapFilterCollectNative(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var result []int
+		for i := 0; i < 10000; i++ {
+			if i%2 == 0 {
+				result = append(result, i*2)
+			}
+		}
+	}
+}
