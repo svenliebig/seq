269ca69a30424979eca346246cec9c6e48b1c223
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2024-04-23 18:18:07 +0200

    refactor reduce to be a collector

--- reduce.go
+++ reduce.go
@@ -1,28 +1,11 @@
 package seq
 
-import (
-	"iter"
-)
+func Reduce[T, U any](s Seq[T], f func(U, T) U) U {
+	var acc U
 
-type reduceSeq[T, U any] struct {
-	f func(U, T) U
-	s iter.Seq[T]
-}
-
-func Reduce[T, U any](s Seq[T], f func(U, T) U) Seq[U] {
-	return reduceSeq[T, U]{f, s.Iterator()}
-}
-
-func (s reduceSeq[T, U]) Iterator() iter.Seq[U] {
-	return func(yield func(U) bool) {
-		var acc U
-
-		for e := range s.s {
-			acc = s.f(acc, e)
-		}
-
-		if !yield(acc) {
-			return
-		}
+	for e := range s.Iterator() {
+		acc = f(acc, e)
 	}
+
+	return acc
 }
--- reduce_test.go
+++ reduce_test.go
@@ -7,65 +7,40 @@ import (
 
 func TestReduce(t *testing.T) {
 	t.Run("should sum integers", func(t *testing.T) {
-		it := Reduce(
+		result := Reduce(
 			Int(0, 5),
 			func(acc, v int) int {
 				return acc + v
 			},
-		).Iterator()
-
-		var result int
-		iterations := 0
-		for v := range it {
-			result = v
-			iterations++
-		}
+		)
 
 		if result != 15 {
 			t.Errorf("Expected 15, got %d", result)
 		}
-
-		if iterations != 1 {
-			t.Errorf("Expected 1 iteration, got %d", iterations)
-		}
 	})
 
 	t.Run("should append integers to a string", func(t *testing.T) {
-		it := Reduce(
+		result := Reduce(
 			Int(0, 5),
 			func(acc string, v int) string {
 				return acc + fmt.Sprint(v)
 			},
-		).Iterator()
-
-		var result string
-		iterations := 0
-		for v := range it {
-			result = v
-			iterations++
-		}
+		)
 
 		if result != "012345" {
 			t.Errorf("Expected 012345, got %s", result)
 		}
-
-		if iterations != 1 {
-			t.Errorf("Expected 1 iteration, got %d", iterations)
-		}
 	})
-
 }
 
 func BenchmarkReduce(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		for v := range Reduce(
+		Reduce(
 			Int(0, 1000),
 			func(acc, v int) int {
 				return acc + v
 			},
-		).Iterator() {
-			_ = v
-		}
+		)
 	}
 	b.ReportAllocs()
 }
